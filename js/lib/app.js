c971efc49b7c6cc8e2d95e93e04518a5d0e0a8da
Prashant Singh <prashant@codeforamerica.org>
2014-02-04 19:30:00 -0800

    Initial commit

---
+++ js/lib/app.js
@@ -0,0 +1,11 @@
+define(function (require) {
+  'use strict';
+
+  var $ = require('jquery');
+
+  var app = {};
+  app.start = function start() {
+  };
+
+  return app;
+});
---
+++ js/main.js
@@ -0,0 +1,7 @@
+require(['lib/app'], function (app) {
+  'use strict';
+
+  $(document).ready(function () {
+    app.start();
+  });
+});
